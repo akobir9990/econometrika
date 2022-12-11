fb7ef78016c0afd6899f8c68a11d28001b1f3718
Akobir <akobir@Akobirs-MacBook-Pro.local>
2022-12-11 13:02:42 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AppBar from './components/appbar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppBar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
