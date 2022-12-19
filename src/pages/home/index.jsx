631821689a198da5b6e9e48749199f2d08443c99
Akobir <akobir@Akobirs-MacBook-Pro.local>
2022-12-19 15:37:54 +0500

    revision of authors page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,23 +3,17 @@ import React from "react";
 function index() {
   return (
     <div>
-      <div className="uppercase">
-        <div className="text-2xl font-semibold">
+      <div className="">
+        <div className="text-4xl font-semibold w-[100%] text-center">
           «Прикладная эконометрика»
         </div>
 
-        <div className="flex flex-wrap w-[100%] border-[1px] border-[solid] border-[red] ">
-          <img
-            className="w-[30%] min-w-[200px] h-[200px] border-[1px] border-[solid] border-[green] "
-            src=""
-            alt="???"
-          />
-          <div className="flex px-[10px] max-w-[600px] min-w-[200px] items-center">
-            <h2 className="border-[1px] border-[solid] border-[blue] ">
-              Автор пособия Мирзаев Сайибджан Сабитович – кандидат технических
-              наук, доцент кафедры «Информационные технологии» Национального
-              исследовательского университета «ТИИИМСХ»
-            </h2>
+        <div className="flex flex-wrap w-[100%]">
+          <div className="my-[100px] w-[100%]">
+            <p>
+              <b>Мирзаев Сайибджан Сабитович–</b> кандидат технических наук, <br />
+              доцент кафедры «Информационные технологии» НИУ «ТИИИМСХ»
+            </p>
           </div>
         </div>
       </div>
